0f8084bb6db11954f967b20c140736a86ba75d15
AndrzejKwiatkowski <andrzej_kwiatkowski@outlook.com>
2020-02-21 09:58:25 +0100

    ON production

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', 'ImportExcelController@index');
 Route::post('/import', 'ImportExcelController@import');
